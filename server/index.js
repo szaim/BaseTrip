a2a4834b1858899aacd75c6c3d7cdae9ba1dda99
mrmojica <mrmojica510@gmail.com>
2017-02-26 16:29:09 -0800

    connected api/xml to json

--- server/index.js
+++ server/index.js
@@ -1,25 +1,42 @@
 const express = require("express");
-const express = require("request");
+const request = require("request");
+const unirest = require("unirest");
+const parser = require("xml2json");
+
 const app = express();
 
 
+let url = "http://api.eventful.com/rest/events/search?app_key=b7BFXqw9rckW5SzC&keywords=books&location=San+Francisco&date=Future";
+
 //Eventful API Key
 // b7BFXqw9rckW5SzC
+// example
+// http://api.eventful.com/rest/events/search?app_key=b7BFXqw9rckW5SzC&keywords=books&location=San+Francisco&date=Future
+// example
+// http://api.eventful.com/rest/categories/list?app_key=b7BFXqw9rckW5SzC
 
-http://api.eventful.com/rest/events/search?app_key=b7BFXqw9rckW5SzC&keywords=books&location=San+Francisco&date=Future
 
-app.get("/", function(req, res){
-let query = ;
-let url =;
+let access_token = "b7BFXqw9rckW5SzC";
 
-request(url, function(error, response, body){
-        if(!error && response.statusCode == 200) {
-            var data = JSON.parse(body)
-            res.send({data});
-        }
-    });
+
+app.get('/api', function(req, res) {
+	let query;
+unirest.get('http://api.eventful.com/rest/events/search?app_key=' + access_token + '&keywords=books&location=San+Francisco&date=Future')
+.end(function(data){
+	let eventData = parser.toJson(data.body);
+
+	return res.send(eventData);
 
 });
+
+});
+
+
+
+
+
+
+
 
 
 app.listen(process.env.PORT || 8080, function(){
