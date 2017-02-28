b3bdb919febeff26bc4705c78294b5217491aaca
mrmojica <mrmojica510@gmail.com>
2017-02-27 17:41:14 -0800

    added foursquare api

--- server/index.js
+++ server/index.js
@@ -6,37 +6,80 @@ const parser = require("xml2json");
 const app = express();
 
 
-let url = "http://api.eventful.com/rest/events/search?app_key=b7BFXqw9rckW5SzC&keywords=books&location=San+Francisco&date=Future";
+let url = "https://api.foursquare.com/v2/venues/explore";
 
 
 // Four Square
 // CLIENT_ID
-// EKBGEVUEK3PRHMIP3XZA5LJRTYVQIJ34IKBGES2VNXAFTOJ0
+let clientID = "EKBGEVUEK3PRHMIP3XZA5LJRTYVQIJ34IKBGES2VNXAFTOJ0";
 // CLIENT_SECRET
-// FK2FTS5XZ511QJAXDMMI0K0NEX3VTWD1HW12ULQZLG0LM1LE
-
-//Eventful API Key
-// b7BFXqw9rckW5SzC
-// example
-// http://api.eventful.com/rest/events/search?app_key=b7BFXqw9rckW5SzC&keywords=books&location=San+Francisco&date=Future
-// example
-// http://api.eventful.com/rest/categories/list?app_key=b7BFXqw9rckW5SzC
+let clientSecret = "FK2FTS5XZ511QJAXDMMI0K0NEX3VTWD1HW12ULQZLG0LM1LE";
+let location = '40.7,-74';
+let date = formatted_date();
 
 
-let access_token = "b7BFXqw9rckW5SzC";
+console.log(date);
+
+// https://api.foursquare.com/v2/venues/search
+//   ?client_id=CLIENT_ID
+//   &client_secret=CLIENT_SECRET
+//   &ll=40.7,-74
+//   &query=sushi
+//   &v=YYYYMMDD
+//   &m=foursquare
+
+// Search (by keyword)
+// https://api.foursquare.com/v2/venues/search?client_id=EKBGEVUEK3PRHMIP3XZA5LJRTYVQIJ34IKBGES2VNXAFTOJ0&client_secret=FK2FTS5XZ511QJAXDMMI0K0NEX3VTWD1HW12ULQZLG0LM1LE&ll=40.7,-74&query=sushi&v=20170227
+
+// Explore
+// https://api.foursquare.com/v2/venues/explore?client_id=EKBGEVUEK3PRHMIP3XZA5LJRTYVQIJ34IKBGES2VNXAFTOJ0&client_secret=FK2FTS5XZ511QJAXDMMI0K0NEX3VTWD1HW12ULQZLG0LM1LE&ll=40.7,-74&query=food&v=20170227
+
+// https://api.foursquare.com/v2/events/categories/food?client_id=EKBGEVUEK3PRHMIP3XZA5LJRTYVQIJ34IKBGES2VNXAFTOJ0&client_secret=FK2FTS5XZ511QJAXDMMI0K0NEX3VTWD1HW12ULQZLG0LM1LE&ll=40.7,-74&v=20170227
 
 
-app.get('/api', function(req, res) {
-	let query;
-unirest.get('http://api.eventful.com/rest/events/search?app_key=' + access_token + '&keywords=books&location=San+Francisco&date=Future')
-.end(function(data){
-	let eventData = parser.toJson(data.body);
+function formatted_date()
+{
+   var result="";
+   var d = new Date();
+   var month = (d.getMonth()+1);
+   var day = d.getDate();
+   if (d.getMonth()+1 < 10){
+   		var month = '0' + (d.getMonth()+1);
+   }
+    else if (d.getDate()+1 < 10){
+   		var day = '0' + d.getDate();
+   }
+   result += d.getFullYear() +''+ month +''+ day;
+   return result;
+}
 
-	return res.send(eventData);
 
+app.get('/', function(req, res) {
+	res.send('BaseTrip');
 });
 
+
+app.get('/venue/search/:search', function(req, res) {
+	let query = req.params.search;
+	unirest.get('https://api.foursquare.com/v2/venues/search?client_id=' + clientID + '&client_secret=' + clientSecret + '&ll=' + location + '&query=' + query + '&v=' + date)
+	.end(function(data){
+		return res.send(data);
+	});
 });
+
+
+app.get('/venue/explore/:explore', function(req, res) {
+	let query = req.params.explore;
+	unirest.get('https://api.foursquare.com/v2/venues/explore?client_id=' + clientID + '&client_secret=' + clientSecret + '&ll=' + location + '&query=' + query + '&v=' + date)
+	.end(function(data){
+		return res.send(data);
+	});
+});
+
+
+
+
+
 
 
 
