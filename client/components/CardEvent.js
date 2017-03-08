c97d05b8f8aecb7686afbcb2e55a85742d761dc8
mrmojica <mrmojica510@gmail.com>
2017-03-07 18:13:22 -0800

    minor css adjustment

--- client/components/CardEvent.js
+++ client/components/CardEvent.js
@@ -5,7 +5,7 @@ const React = require('react');
 const CardEvent = function(props) {
 	var link = '300x500';
 	return (
-		<div className="col-xs-6 card-animation" style={{'paddingLeft': 0}}>
+		<div className="col-xs-6 card-animation" id={props.key} style={{'paddingLeft': 0}}>
 			<img className="event-img" src={`${props.prefix}300x500${props.suffix}`} />
 			<div>
 				<span className='event-title'>{props.title}</span><br />
