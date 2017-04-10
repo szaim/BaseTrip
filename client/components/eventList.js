83c04576f53f99bafd5099de12a7078f36304048
mrmojica <mrmojica510@gmail.com>
2017-04-10 15:34:44 -0700

    added loader to eventListPage

--- client/components/eventList.js
+++ client/components/eventList.js
@@ -1,16 +1,36 @@
 const React = require('react');
+const connect = require('react-redux').connect;
 const SearchHeader = require('./container/SearchHeader');
 const Category = require('./container/Category');
 const Footer = require('./footer');
+const Loader = require('./loader');
 
-const EventList = function() {
-	return (
+const EventList = React.createClass({
+	getInitialState: function() {
+		return {
+			loading: true
+		};
+	},
+	componentWillMount: function() {
+			setTimeout(() => {
+				this.setState({loading: false});
+			}, 3000);
+	},
+	render: function() {	
+	let content = this.state.loading ? <div><SearchHeader /> <Loader /></div> : 
 		<div>
 			<SearchHeader />
 			<Category />
 			<Footer directLink='https://blooming-ocean-74461.herokuapp.com/' brandName='Base Trip' />
 		</div>
+	return (
+		<div>
+			{content}
+		</div>
 	)
 }
+});
 
-module.exports = EventList;
+const Container = connect()(EventList);
+
+module.exports = Container;
